c95e922a4392a9e81fc44b970c7de15375e3d8da
yongjian.cyj <yongjian.cyj@dtwave-inc.com>
2020-04-22 14:48:44 +0800

    update

---
+++ Robot/V1.0.0/files/产品功能结构/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(bf,_(bg,bh,bi,bj),w,bk),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="产品功能结构.html",d="generationDate",e=new Date(1587538073123.33),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="68a7b5e115054e70a187d65b8d7cec1e",q="type",r="Axure:Page",s="name",t="产品功能结构",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="70970960e81b45f2a7c0362166406438",Y="label",Z="friendlyType",ba="图片 ",bb="imageBox",bc="styleType",bd="visible",be=true,bf="size",bg="width",bh=1208,bi="height",bj=3135,bk="75a91ee5b9d042cfa01b8d565fe289c0",bl="imageOverrides",bm="images",bn="normal~",bo="images/产品功能结构/u61.png",bp="masters",bq="objectPaths",br="70970960e81b45f2a7c0362166406438",bs="scriptId",bt="u61";
+return _creator();
+})());
