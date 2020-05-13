a9c2557d23f9ed4826c25a7dc470e242cbdc319a
yongjian.cyj <yongjian.cyj@dtwave-inc.com>
2020-05-13 10:01:26 +0800

    update

---
+++ LeGao/V2.0.1/files/我的报表优化/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(w,bf,bg,_(bh,bi,bj,bk)),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="我的报表优化.html",d="generationDate",e=new Date(1589335248333.81),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="3442a38d6a4a4818a60e6f4de772acad",q="type",r="Axure:Page",s="name",t="我的报表优化",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="bc7c1a5b81a24916b102ee57d90c6ed2",Y="label",Z="friendlyType",ba="图片 ",bb="imageBox",bc="styleType",bd="visible",be=true,bf="75a91ee5b9d042cfa01b8d565fe289c0",bg="size",bh="width",bi=1438,bj="height",bk=821,bl="imageOverrides",bm="images",bn="normal~",bo="images/我的报表优化/u480.jpg",bp="masters",bq="objectPaths",br="bc7c1a5b81a24916b102ee57d90c6ed2",bs="scriptId",bt="u480";
+return _creator();
+})());
